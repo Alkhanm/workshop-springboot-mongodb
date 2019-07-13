73788193227d0ffe58ff9e26c2edb7ff419090ea
Alkhanm <joas7777777@hotmail.com>
2019-07-13 15:25:18 -0300

    Obtendo um úsuario por ID

--- src/main/java/com/alkhanm/workshopmongo/services/UserService.java
+++ src/main/java/com/alkhanm/workshopmongo/services/UserService.java
@@ -1,10 +1,13 @@
 package com.alkhanm.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.alkhanm.workshopmongo.domain.User;
 import com.alkhanm.workshopmongo.repository.UserRepository;
+import com.alkhanm.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -15,4 +18,8 @@ public class UserService {
 	public List<User> findAll(){
 		return repo.findAll();
 	}
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		}
 }
