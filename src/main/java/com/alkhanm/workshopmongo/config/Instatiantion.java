9f6609a7ec9467eeb46ba37ed3ec4c327b283da9
Alkhanm <joas7777777@hotmail.com>
2019-07-30 12:29:40 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/alkhanm/workshopmongo/config/Instatiantion.java
+++ src/main/java/com/alkhanm/workshopmongo/config/Instatiantion.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.alkhanm.workshopmongo.DTO.AuthorDTO;
 import com.alkhanm.workshopmongo.domain.Post;
 import com.alkhanm.workshopmongo.domain.User;
 import com.alkhanm.workshopmongo.repository.PostRepository;
@@ -34,16 +35,18 @@ public class Instatiantion implements CommandLineRunner{
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
+		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		
+		Post post = new Post(null, sdf.parse("21/03/2018"), "Partiu, viagem!", "Ai galera, bora?", new AuthorDTO(bob));
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Bora, poha", "Tamo junto!", new AuthorDTO(bob));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Ow", "Vou tbm.", new AuthorDTO(bob));
 		
-		Post post = new Post(null, sdf.parse("21/03/2018"), "Partiu, viagem!", "Ai galera, bora?", bob);
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Bora, poha", "Tamo junto!", bob);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Ow", "Vou tbm.", bob);
+		
 		postRepository.saveAll(Arrays.asList(post, post1, post2));
 		
-		bob.getPosts().addAll(Arrays.asList(post, post1, post2));
-		userRepository.save(bob);
+		//bob.getPosts().addAll(Arrays.asList(post, post1, post2));
+		//userRepository.save(bob);
 		
 	}
 }
