8ccefc7db1d086859193a441b6f8e9dc78e176c3
Alkhanm <joas7777777@hotmail.com>
2019-07-31 14:50:26 -0300

    Consulta com @Query

--- src/main/java/com/alkhanm/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alkhanm/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,17 @@ package com.alkhanm.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.alkhanm.workshopmongo.domain.Post;
 
 @Repository                                            
 public interface PostRepository extends MongoRepository<Post, String> {//Associa o repositorio a classe Post com o Id em String.
+	
+	//Usa o campo titulo, pegando o primeiro campo(text) que está no indice 0. E com a opção IgnoreCase representado pelo 'i'
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
